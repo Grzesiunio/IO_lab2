b7946bbb549c947ea1988d8f1a30ef1336ef2615
Grzegorz Drozda <gregordr123@gmail.com>
2020-11-02 16:37:20 +0100

    add operation

---
+++ IO_lab2_serwer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using ServerEchoLib;
+
+namespace IO_lab2_serwer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerEchoAPM server = new ServerEchoAPM(IPAddress.Parse("127.0.0.1"), 7777);
+            try
+            {
+                server.Port = -1;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            server.Start();
+        }
+    }
+}
